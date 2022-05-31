1d8a7147a6fae1c07d8f79bcafcd2aad264254a0
kendalelci <kendalelci@gmail.com>
2022-05-31 16:56:32 -0700

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -4,6 +4,9 @@ public class gitAndGithub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("today we learn github");
+		System.out.println("Today is a very good day");
 
 	}
 
